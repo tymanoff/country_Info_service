1c0e7aba63d9287af6a7b9f2ee0c838a0cc0390f
tymanoff <djdkdancik@gmail.com>
2022-10-30 11:41:38 +0400

    Initial commit

---
+++ src/main/java/ru/info/country/CountryServiceApplication.java
@@ -0,0 +1,13 @@
+package ru.info.country;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CountryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CountryServiceApplication.class, args);
+	}
+
+}
