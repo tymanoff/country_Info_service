b24f966dc62522df7863aba428f521c9bdbe89e0
tymanoff <djdkdancik@gmail.com>
2022-10-31 21:19:58 +0400

    add test, nginx, env, docker-compose, checkstyle, spotbugs, Dockerfile and connect to db

--- src/main/java/ru/info/country/CountryServiceApplication.java
+++ src/main/java/ru/info/country/CountryServiceApplication.java
@@ -2,12 +2,13 @@ package ru.info.country;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class CountryServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CountryServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(CountryServiceApplication.class, args);
+    }
 
 }
