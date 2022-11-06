43ea78786f793db930972bfc4b39d3a26899cd2f
tymanoff <djdkdancik@gmail.com>
2022-11-06 17:49:48 +0400

    add compile, add SoapTemplateConfig, add CountryServiceProperties, add xml

--- src/main/java/ru/info/country/CountryServiceApplication.java
+++ src/main/java/ru/info/country/CountryServiceApplication.java
@@ -3,8 +3,11 @@ package ru.info.country;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import ru.info.country.configuration.CountryServiceProperties;
 
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@EnableConfigurationProperties(CountryServiceProperties.class)
 public class CountryServiceApplication {
 
     public static void main(String[] args) {
